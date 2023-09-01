2b108ae962fd62f8b7acdfbaa331c79d1ccf3f70
Muhammad Ali Zahid <mohammad.aliz.gg@gmail.com>
2023-09-01 15:12:36 +0500

    new packages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 // import logo from './logo.svg';
 import './App.css';
-import GaussianBlurWrapper from './components/GaussianBlurWrapper';
+
+
+import React, { useState } from 'react';
 
 function App() {
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  const handleNext = () => {
+    // handle next button click
+    console.log('Next button clicked');
+  };
+
   return (
-    <div>
-      <h1 className='center'>Hello World</h1>
-      <GaussianBlurWrapper />
+    <div className='center-container'>
+      <h1 className='center'>Select Number of Subjects</h1>
+      <select value={selectedValue} onChange={handleChange} className='center-dropdown'>
+        <option value='' disabled>
+          None
+        </option>
+        {[...Array(9)].map((_, i) => (
+          <option key={i} value={i + 1}>
+            {i + 1}
+          </option>
+        ))}
+      </select>
+      <button className='next-button' onClick={handleNext}>Next</button>
     </div>
   );
 }
-
 export default App;
