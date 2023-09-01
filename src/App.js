224b51fca0fb9db4b90fea45859a0477ca83b499
Muhammad Ali Zahid <mohammad.aliz.gg@gmail.com>
2023-09-01 14:33:52 +0500

    basic files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import GaussianBlurWrapper from './components/GaussianBlurWrapper';
+
+function App() {
+  return (
+    <div>
+      <h1 className='center'>Hello World</h1>
+      <GaussianBlurWrapper />
+    </div>
+  );
+}
+
+export default App;
