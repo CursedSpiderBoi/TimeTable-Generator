ab9f6ff8045e0decd24c53dca3072960b38a33ad
Muhammad Ali Zahid <mohammad.aliz.gg@gmail.com>
2023-09-02 11:45:08 +0500

    stuff

--- src/functionalities.js
+++ src/functionalities.js
@@ -3,35 +3,21 @@ const fs = require('fs');
 
 const workbook = xlsx.readFile("./assets/FSC_Time_Table__List_of_Courses_Fall_2023_v1.5.xlsx");
 
-function convertToDataSheet() {
-    const sheetName = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[sheetName];
-    const data = xlsx.utils.sheet_to_json(worksheet).filter(row => !Object.values(row).some(cell => typeof cell === 'string' && cell.includes('__EMPTY_')));
-    const filePath = 'data.json';
-    if (fs.existsSync(filePath)) {
-        fs.truncateSync(filePath, 0);
-    }
-    fs.writeFileSync(filePath, JSON.stringify(data));
+const sheet_name_list = workbook.SheetNames;
+
+function readSheetData(sheetIndex) {
+    const worksheet = workbook.Sheets[sheet_name_list[sheetIndex]];
+    const rows = xlsx.utils.sheet_to_json(worksheet, { header: 1, defval: null });
+    const data = rows.filter(row => row.some(cell => cell !== null));
+    return data;
 }
 
-function sortDataSheet() {
-    const dataSheet = require('../data.json');
-    const sortedDataSheet = {};
-    for (const sheetName in dataSheet) {
-        const data = dataSheet[sheetName];
-        const sortedData = data.sort((a, b) => {
-            if (a.Course_Code < b.Course_Code) {
-                return -1;
-            }
-            if (a.Course_Code > b.Course_Code) {
-                return 1;
-            }
-            return 0;
-        });
-        sortedDataSheet[sheetName] = sortedData;
-    }
-    fs.writeFileSync('./data/data.json', JSON.stringify(sortedDataSheet));
+function writeDataToFile(data) {
+    const content = `module.exports = ${JSON.stringify(data)};`;
+    fs.writeFile('./data.js', content, (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    });
 }
 
-convertToDataSheet();
-// sortDataSheet();
+writeDataToFile(readSheetData(0));
